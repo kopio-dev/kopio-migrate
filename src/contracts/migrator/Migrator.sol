b7ce4500ff32d2f5fe87a2cb0f696c0107453bf3
fxp3 <dev@0xp.lol>
2024-08-19 15:27:54 +0300

    feat: add base

---
+++ src/contracts/migrator/Migrator.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {IKreditsDiamond} from "kresko/core/IKreditsDiamond.sol";
+import {IKresko} from "kresko/core/IKresko.sol";
+import {Cutter} from "kopio/vm-ffi/Cutter.s.sol";
+import {Based} from "kopio/vm/Based.s.sol";
+
+contract KrBase is Cutter, Based {
+    address[] testAccs = [
+        0x5a6B3E907b83DE2AbD9010509429683CF5ad5984, // dev
+        0x99999A0B66AF30f6FEf832938a5038644a72180a, // self
+        0x7e04f2812F952fB16df52C25aAefb96fcA7c8574, // miko
+        0xADDB385343d851d92CC8639162e5aD18c16E47Df, // ayuush
+        0xada18123Bf1788119Dd557DaCA618a2e92e1BE3c, // self
+        0x13458523bFCb8F0c30C440e77945C392AaA5020f, // qual
+        0xd6bEDEcDeC6Dc1C5900abeFD2CAB1663BCED8E22, // akira
+        0x3dD318bE619FaaedCF49D582Efa9fb087C688670, // george?
+        0x0E3DE118782bC7e4C7AFaFdD29Af762F4CdEcab5, // fenix
+        0xFcbB93547B7C1936fEbfe56b4cEeD9Ab66dA1857, // dev
+        0x36d50Cf7b7dfac786f3F14d251299F0593517E17, // miko
+        0x361Bae08CDd251b022889d8eA9fb8ddb84012516
+    ];
+    IKresko constant kresko = IKresko(kreskoAddr);
+    IKreditsDiamond constant kredits = IKreditsDiamond(kreditsAddr);
+}
---
+++ src/contracts/migrator/kresko/KrContracts.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+// solhint-disable no-inline-assembly, one-contract-per-file, state-visibility, const-name-snakecase
+pragma solidity ^0.8.0;
+
+library kr {
+    address constant kissAddr = 0x6A1D6D2f4aF6915e6bBa8F2db46F442d18dB5C9b;
+    address constant kreskoAddr = 0x0000000000177abD99485DCaea3eFaa91db3fe72;
+    address constant kreditsAddr = 0x8E84a3B8e0b074c149b8277c753Dc6396bB95F48;
+    address constant multicallAddr = 0xC35A7648B434f0A161c12BD144866bdf93c4a4FC;
+    address constant factoryAddr = 0x000000000070AB95211e32fdA3B706589D3482D5;
+    address constant vaultAddr = 0x2dF01c1e472eaF880e3520C456b9078A5658b04c;
+    address constant dataAddr = 0xef5196c4bDd74356943dcC20A7d27eAdD0F9b9D7;
+    address constant marketStatusAddr =
+        0xf6188e085ebEB716a730F8ecd342513e72C8AD04;
+
+    address constant krETHAddr = 0x24dDC92AA342e92f26b4A676568D04d2E3Ea0abc;
+    address constant krBTCAddr = 0x11EF4EcF3ff1c8dB291bc3259f3A4aAC6e4d2325;
+    address constant krSOLAddr = 0x96084d2E3389B85f2Dc89E321Aaa3692Aed05eD2;
+    address constant krEURAddr = 0x83BB68a7437b02ebBe1ab2A0E8B464CC5510Aafe;
+    address constant krJPYAddr = 0xc4fEE1b0483eF73352447b1357adD351Bfddae77;
+    address constant krGBPAddr = 0xdb274afDfA7f395ef73ab98C18cDf3D9C03b538C;
+    address constant krXAUAddr = 0xe0A49C9215206f9cfb79981901bDF1f2716d3215;
+    address constant krXAGAddr = 0x1d6A65BBfbbc995a19Fc19cB17FA135f9EdB6A24;
+    address constant krDOGEAddr = 0x4a719F02aF3f0FFf15447B6824464857ADB5210D;
+
+    address constant akrETHAddr = 0x3103570A28ca026e818c79608F1FF804F4Bde284;
+    address constant akrBTCAddr = 0xc67a33599f73928D24D32fC0015e187157233410;
+    address constant akrSOLAddr = 0x362cB60d235Cf8258042DAfB2a3Cdb14302D9D0f;
+    address constant akrEURAddr = 0xBb6053898C5f6e536405fA324839141aA102b6D9;
+    address constant akrJPYAddr = 0x3438Eb57e5b0f1CbEca257Aea9644B26b1B61EaC;
+    address constant akrGBPAddr = 0x37BddA32281c15716D35f901b8141f7F382220C1;
+    address constant akrXAUAddr = 0x3A1ffd3426916B16878AAa072B74DdaEC3e31007;
+    address constant akrXAGAddr = 0x4d516E2049542B350368A44cBE71F3bbc00000D6;
+    address constant akrDOGEAddr = 0x44217deFe47C3F5D03471d59723CF437efBfb871;
+
+    address constant safe = 0x266489Bde85ff0dfe1ebF9f0a7e6Fed3a973cEc3;
+    address constant nftMultisig = 0x389297F0d8C489954D65e04ff0690FC54E57Dad6;
+    address constant kreskianAddr = 0xAbDb949a18d27367118573A217E5353EDe5A0f1E;
+    address constant questAddr = 0x1C04925779805f2dF7BbD0433ABE92Ea74829bF6;
+}
